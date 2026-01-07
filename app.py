f6ac126a7c322061e3db63b811165cd340b7482e
adithya0814 <adithyaadhi513@gmail.com>
2026-01-07 14:24:56 +0530

    Rename main.py to app.py to match Render default start command

--- app.py
+++ app.py
@@ -302,4 +302,4 @@ async def predict_all_endpoint(data: AllFeatures, request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
